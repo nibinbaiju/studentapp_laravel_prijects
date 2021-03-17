d44d19998c402899e9c94aae0b4167fdf9d2289f
nibinbaiju <nibinbaiju@gmail.com>
2021-03-17 11:53:43 +0530

    raw

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+</div>
+<div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+<table class="table">
+<tr>
+    <td>Name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>College</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>ROLL NO</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>AdmNo</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+
+
+</table>
+</div>
+<div class="<div col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4"></div>
+
+
+</div>
+</div>   
+</body>
+</html>
